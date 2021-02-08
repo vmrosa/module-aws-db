5e88ebbdf0af89ce5bcfa1f60d56543981b92552
vmrosa <47866729+vmrosa@users.noreply.github.com>
2021-02-08 08:54:32 -0300

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,13 @@
+/*
+output "mysql_instance_id" {
+  value = aws_db_instance.mysql-db.id
+}
+
+output "mysql_endpoint" {
+  value = aws_db_instance.mysql-db.endpoint
+}
+*/
+
+#output "redis_instance_id" {
+#  value = aws_elasticache_cluster.ms_redis
+#}
